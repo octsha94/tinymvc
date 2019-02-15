151a63a33b3333903bc45eaee86dea7ddcf85ed9
octsha94 <47352772+octsha94@users.noreply.github.com>
2019-02-15 15:45:53 +0300

    Добавлен ini_set
    Добавлен файл конфигурации сервера

---
+++ application/ini_set.php
@@ -0,0 +1,7 @@
+<?php
+
+defined('IN_SYSTEM') or die('Запрет доступа!');
+
+ini_set('error_reporting', 32767);
+ini_set('session.save_path', ROOT.'temp/');
+ini_set('date.timezone', 'Europe/Moscow');
