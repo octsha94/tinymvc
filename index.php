94199d0babce4ae86135cce229b68fef54502bb6
octsha94 <47352772+octsha94@users.noreply.github.com>
2019-02-15 15:54:22 +0300

    Переработана логика файла index.php

--- index.php
+++ index.php
@@ -1,4 +1,26 @@
-﻿<?php
+<?php
 
-ini_set('display_errors', 1);
-require_once 'application/bootstrap.php';
+//абсолютный путь директория файла
+defined('ROOT') or define('ROOT', str_replace('\\', '/', realpath(dirname(__FILE__))).'/');
+
+//системная переменная
+define('IN_SYSTEM', TRUE);
+
+//стартуем или возобновляем сессию
+session_name('sid');
+session_start();
+
+require_once ('application/functions.php');
+
+//чистим GET-массив
+$_GET = array_map('safe_array', $_GET);
+
+//подключаем файлы ядра
+autoload('ini_set');
+autoload('core/controller');
+autoload('core/model');
+autoload('core/view');
+autoload('core/route');
+
+//запускаем маршрутизатор
+Route::start();
