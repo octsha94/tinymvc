2fc6e26cb21c2b5bafe023d6ea59eddb2bee6735
octsha94 <47352772+octsha94@users.noreply.github.com>
2019-02-15 15:42:08 +0300

    Переработан механизм маршрутизации
    Добавлена защита от прямого вызова файла
    Добавлен маршрут для ID действия 
    Добавлены регулярные выражения для фильтрации маршрутов
    Внесены мелкие правки по организации кода

--- application/core/route.php
+++ application/core/route.php
@@ -1,51 +1,49 @@
 <?php
 
-/*
-Класс-маршрутизатор для определения запрашиваемой страницы.
-> цепляет классы контроллеров и моделей;
-> создает экземпляры контролеров страниц и вызывает действия этих контроллеров.
-*/
+defined('IN_SYSTEM') or die('Запрет доступа!');
+
+//класс маршрутизатор
+
 class Route
 {
-
 	static function start()
 	{
-		// контроллер и действие по умолчанию
+		//контроллер, действие и ID действия по умолчанию
 		$controller_name = 'Main';
 		$action_name = 'index';
+		$action_id = NULL;
 		
-		$routes = explode('/', $_SERVER['REQUEST_URI']);
+		$routes = explode('/', $_SERVER['REQUEST_URI'], 4);
 
-		// получаем имя контроллера
-		if ( !empty($routes[1]) )
+		//имя контроллера
+		if (!empty($routes[1]))
 		{	
-			$controller_name = $routes[1];
+			$controller_name = preg_replace('/[^a-zA-Z0-9]/', '', $routes[1]);
 		}
 		
-		// получаем имя экшена
-		if ( !empty($routes[2]) )
+		//имя действия
+		if (!empty($routes[2]))
 		{
-			$action_name = $routes[2];
+			$action_name = preg_replace('/[^a-zA-Z0-9]/', '', $routes[2]);
 		}
 
-		// добавляем префиксы
+		//ID действия
+        if (!empty($routes[3]))
+        {
+            $action_name = preg_replace('/[^a-zA-Z0-9]/', '', $routes[3]);
+        }
+
+		//добавляем префиксы
 		$model_name = 'Model_'.$controller_name;
 		$controller_name = 'Controller_'.$controller_name;
 		$action_name = 'action_'.$action_name;
 
-		/*
-		echo "Model: $model_name <br>";
-		echo "Controller: $controller_name <br>";
-		echo "Action: $action_name <br>";
-		*/
-
-		// подцепляем файл с классом модели (файла модели может и не быть)
-
+		//подцепляем файл с классом модели, если есть
 		$model_file = strtolower($model_name).'.php';
 		$model_path = "application/models/".$model_file;
 		if(file_exists($model_path))
 		{
-			include "application/models/".$model_file;
+            include_once "application/models/".$model_file;
 		}
 
 		// подцепляем файл с классом контроллера
@@ -53,14 +51,11 @@ class Route
 		$controller_path = "application/controllers/".$controller_file;
 		if(file_exists($controller_path))
 		{
-			include "application/controllers/".$controller_file;
+            include_once "application/controllers/".$controller_file;
 		}
 		else
 		{
-			/*
-			правильно было бы кинуть здесь исключение,
-			но для упрощения сразу сделаем редирект на страницу 404
-			*/
+
 			Route::ErrorPage404();
 		}
 		
@@ -71,11 +66,10 @@ class Route
 		if(method_exists($controller, $action))
 		{
 			// вызываем действие контроллера
-			$controller->$action();
+			$controller->$action($action_id);
 		}
 		else
 		{
-			// здесь также разумнее было бы кинуть исключение
 			Route::ErrorPage404();
 		}
 	
