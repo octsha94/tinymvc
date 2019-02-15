634683b83ad8be2a7827d1e0de3ac9f62b9a18b9
octsha94 <47352772+octsha94@users.noreply.github.com>
2019-02-15 14:46:53 +0300

    Добавлена защита от прямого вызова файла
    Внесены мелкие правки по организации кода

--- application/core/view.php
+++ application/core/view.php
@@ -1,31 +1,23 @@
 <?php
 
+defined('IN_SYSTEM') or die('Запрет доступа!');
+
 class View
 {
-	
-	//public $template_view; // здесь можно указать общий вид по умолчанию.
-	
 	/*
-	$content_file - виды отображающие контент страниц;
-	$template_file - общий для всех страниц шаблон;
-	$data - массив, содержащий элементы контента страницы. Обычно заполняется в модели.
+	$content_file - контент страницы
+	$template_file - общий для всех страниц представление
+	$data - массив, содержащий элементы контента страницы
 	*/
 	function generate($content_view, $template_view, $data = null)
 	{
 		
-		/*
+
 		if(is_array($data)) {
-			
-			// преобразуем элементы массива в переменные
+			//импорт переменных из массива
 			extract($data);
 		}
-		*/
-		
-		/*
-		динамически подключаем общий шаблон (вид),
-		внутри которого будет встраиваться вид
-		для отображения контента конкретной страницы.
-		*/
-		include 'application/views/'.$template_view;
+
+        include_once 'application/views/'.$template_view;
 	}
 }
